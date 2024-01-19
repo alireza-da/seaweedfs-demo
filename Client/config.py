9594c428e9f2bec940d53ed1e2cde70a01873698
Alireza Davoodi <alirezadavoodi1378@gmail.com>
2024-01-19 19:20:38 +0330

    linting fix

--- Client/config.py
+++ Client/config.py
@@ -1,6 +1,9 @@
 import yaml
 
 class Config:
+    """
+    Configuration write/read using yaml library. Keep this class untouched
+    """
     data = None
     def __init__(self) -> None:
         with open('config.yaml', 'r', encoding='UTF-8') as file:
